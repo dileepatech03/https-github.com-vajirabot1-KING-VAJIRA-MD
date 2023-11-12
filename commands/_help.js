5b34ae5ce61a8dbb3233ad33b88a78e178903112
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-12 10:30:29 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/_help.js
+++ commands/_help.js
@@ -20,75 +20,94 @@ const Secktor = require('../lib/commands')
 
     //---------------------------------------------------------------------------
 Secktor.cmd({
-            pattern: "help",
-            alias: ["menu"],
-            desc: "Help list",
-            category: "general",
-            react: "🧑‍💻",
-            filename: __filename
+            pattern: "menu",
+            react: "📃",
+            category: "menu1",
+            filename: __filename,
+            desc: "is bot alive??"
         },
-        async(Void, citel, text) => {
-            const { commands } = require('../lib');
-            if (text.split(" ")[0]) {
-                let arr = [];
-                const cmd = commands.find((cmd) => cmd.pattern === (text.split(" ")[0].toLowerCase()))
-                if (!cmd) return await citel.reply("*❌No Such commands.*");
-                else arr.push(`*🍁Command:* ${cmd.pattern}`);
-                if (cmd.category) arr.push(`*🧩Category:* ${cmd.category}`);
-                if (cmd.alias) arr.push(`*🧩Alias:* ${cmd.alias}`);
-                if (cmd.desc) arr.push(`*🧩Description:* ${cmd.desc}`);
-                if (cmd.use) arr.push(`*〽️Usage:*\n \`\`\`${prefix}${cmd.pattern} ${cmd.use}\`\`\``);
-                return await citel.reply(arr.join('\n'));
-            } else {
-                const cmds = {}
-                commands.map(async(command, index) => {
-                    if (command.dontAddCommandList === false && command.pattern !== undefined) {
-                        if (!cmds[command.category]) cmds[command.category] = []
-                        cmds[command.category].push(command.pattern)
-                    }
-                })
-                const time = moment(moment())
-                    .format('HH:mm:ss')
-                moment.tz.setDefault('Asia/KOLKATA')
-                    .locale('id')
-                const date = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
-                let total = await sck1.countDocuments()
-                let str = `────⦁🍁 ` + fancytext(Config.ownername.split('KING VAJIRA MD')[0], 58) + ` 🍁⦁────\n`
-                str +=
-                    '' + `💓𝗵𝗲𝗹𝗹𝗼𝘄 ${citel.pushName}💓
-╭───────────●╮
-╎☀ 𝗴𝗼𝗼𝗱 𝗺𝗼𝗿𝗻𝗶𝗻𝗴
-╰───────────●╯
-╔══════════❍╮
-║🔸𝐎𝐖𝐍 𝐍𝐀𝐌𝐄 ➻ᴡᴍʀ•ᴠᴀᴊɪʀᴀ
-║▪️𝐎𝐖𝐍 𝐍𝐔𝐁 ➻+94766943622
-║🔹𝐁𝐎𝐓 𝐒𝐏𝐄𝐄𝐃 ➻000.23
-║▫️𝐌𝐄𝐌𝐎𝐑𝐘 ➻64GB
-╚══════════❍╯
-    ╭─────────●
-    ╎𝙘𝙤𝙢𝙢𝙖𝙣𝙙 𝙡𝙞𝙨𝙩
-    ╰─────────●\n
-` + ''
-                for (const category in cmds) 
-                {
-                   str += `╔═❋${tiny(category)}❋══❍\n` ;                   if(text.toLowerCase() == category.toLowerCase()){ str = `╔═❋ ${tiny(category)}\n❋══❍` ;      
-                        for (const plugins of cmds[category]) { str += `║🍁⃞➢ ${fancytext(plugins,1)}\n` ; }
-                        str += `╚════════════☉⚟\n`  ;
-                        break ;
-                   }
-                   else { for (const plugins of cmds[category]) { str += `║ 🎋► ${fancytext(plugins,1)}\n` ; }
-                         str += `╚══════════☉⚟\n`  ; 
-                   }
-  
-                }
-                str+= `ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ᴠᴀᴊɪʀᴀ 
-ᴋɪɴɢ ᴠᴀᴊɪʀᴀ ᴍᴅ`
-                let buttonMessaged = {
-                    image: { url: await botpic() },
-                    caption: str
-                };
-                return await Void.sendMessage(citel.chat, buttonMessaged);
-            }
+        async(Void, citel, text, isAdmins) => {
+            let alivemessage = Config.ALIVE_MESSAGE || `*A bot developed by Dumidu.*`
+            const alivtxt = `
+```👋 Hello Vajira Rathnayaka```
+
+```🫧 King Vajira Commands Menu``` 
+
+📅 12/11/2023
+⏰ 02:48:12  
+📡 Uptime: 10 hours, 17 minutes, 15 seconds
+🚁 Platform : Heroku
+🧑‍💻 Developer: *Vajiraa Rathnayaka*
+🐋 Web : *https://bit.ly/King vajira*
+
+🔢 Reply below number ,
+
+*1 | ʀᴇᴀᴄᴛɪᴏɴ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*2 | ᴀɴɪᴍᴇ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*3 | ᴇxᴛʀᴀ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*4 | ɢᴀᴍᴇꜱ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*5 | ɢᴀᴍᴇ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*6 | ꜰᴜɴ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*7 | ɢᴇɴᴇʀᴀʟ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*8 | ᴛᴏᴏʟ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*9 | ᴅᴏᴡɴ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*10 | ᴜᴛɪʟɪᴛʏ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*11 | ʟɪꜱᴛ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*12 | ᴛᴇxᴛᴘʀᴏ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*13 | ᴄᴏɴᴠᴇʀᴛ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*14 | ᴍɪꜱᴄ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*15 | ꜱᴛɪᴄᴋᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*16 | ᴅᴏᴡɴ2 ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*17 | ᴇᴄᴏɴᴏᴍʏ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*18 | ᴜꜱᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*19 | ᴀɪ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*20 | ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*21 | ɢʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*22 | ꜰʀᴀɴᴋ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*23 | ɴᴇᴡꜱ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*24 | ꜱᴇᴀʀᴄʜ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+*25 | ᴍᴏᴅᴇʀᴀᴛ ᴄᴏᴍᴍᴀɴᴅꜱ*
+
+
+ᴋɪɴɢ ᴠᴀᴊɪʀᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ
+ꜱᴛᴀʙʟᴇ ᴠᴇʀꜱɪᴏɴ : ɪɪ
+`;
+            let aliveMessage = {
+                image: {
+                    url: await botpic(),
+                },
+                caption: alivtxt,
+                footer: tlang().footer,
+                headerType: 4,
+            };
+             return Void.sendMessage(citel.chat, aliveMessage, {
+                quoted: citel,
+            });
+
         }
     )
     //---------------------------------------------------------------------------
