185153f8625f9a9fabc56d64732115365745d7ce
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-14 09:23:10 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- src/downloader.js
+++ src/downloader.js
@@ -527,7 +527,7 @@ if (text.startsWith("https://youtube.com/shorts/")) {
 ┋🧑‍🎤 *Author:* ${anu.author.name}
 ┋⬇️ Upload To Song
  ───────➢────────
-⦿ *Url* : ${anu.url}\n\n\nReply 1 To Audio \nReply 2 To Document" ;
+⦿ *Url* : ${anu.url}
 `,			
                 footer: tlang().footer,
                 headerType: 4,
