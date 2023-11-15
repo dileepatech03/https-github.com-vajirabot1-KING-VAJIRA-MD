94217058a4ed50a7207f05b385dfdcaeffcca362
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-15 07:25:19 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/_help.js
+++ commands/_help.js
@@ -12,11 +12,11 @@
 const os = require('os')
 const moment = require("moment-timezone")
 const fs = require("fs")
-const Config = require('../settings')
+const Config = require('../config')
 let { fancytext, tlang, tiny, runtime, formatp, botpic, prefix, sck1 } = require("../lib");
 const long = String.fromCharCode(8206)
 const readmore = long.repeat(4001)
-const Secktor = require('../lib/src')
+const Secktor = require('../lib/commands')
 
     //---------------------------------------------------------------------------
 Secktor.cmd({
