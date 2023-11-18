8584f0f636d3ed7d13ed77121e4ffb87ec880f6a
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-18 19:59:33 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- plugin/Wa.js
+++ plugin/Wa.js
@@ -1,20 +1,3 @@
-/**
-██╗███████╗██╗   ██╗██╗  ██╗██╗   ██╗    ███╗   ███╗██████╗ 
-██║╚══███╔╝██║   ██║██║ ██╔╝██║   ██║    ████╗ ████║██╔══██╗
-██║  ███╔╝ ██║   ██║█████╔╝ ██║   ██║    ██╔████╔██║██║  ██║
-██║ ███╔╝  ██║   ██║██╔═██╗ ██║   ██║    ██║╚██╔╝██║██║  ██║
-██║███████╗╚██████╔╝██║  ██╗╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
-╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
-
- Copyright (C) 2023.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : IZUKU-Md
- * @author : excel <https://github.com/excelottah6>
- * @description : IZUKU,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
 
 const { sck, sck1,cmd, jsonformat, botpic, TelegraPh, RandomXP, Config, tlang, warndb, sleep,getAdmin,getBuffer, prefix } = require('../lib')
 const moment = require("moment-timezone");
