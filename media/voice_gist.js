b94cbb77040e64fa468443e760d8e7c9c54ca377
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-24 16:15:50 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- media/voice_gist.js
+++ media/voice_gist.js
@@ -1,1 +1,9 @@
-
+{
+"hi": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/hi.mp3?raw=true",
+"bye": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/bye.mp3?raw=true",
+"gn": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/gn.mp3?raw=true",
+"gm": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/gm.mp3?raw=true",
+"bs": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/bs.mp3?raw=true",
+"mk": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/mk.mp3?raw=true",
+"menu": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/menu.mp3?raw=true"
+}
