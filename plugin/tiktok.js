b30dfa2beb2c46b15d785a26d5404f2a59f67752
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-25 21:40:30 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- plugin/tiktok.js
+++ plugin/tiktok.js
@@ -9,6 +9,15 @@
 //---------------------------------------------------------------------------
 **/
 
+const { tlang, ringtone, cmd,fetchJson, sleep, botpic,ffmpeg, getBuffer, pinterest, prefix, Config } = require('../lib')
+const { mediafire } = require("../lib/mediafire.js");
+const googleTTS = require("google-tts-api");
+const ytdl = require('ytdl-secktor')
+const fs = require('fs-extra')
+var videotime = 60000 // 1000 min
+var dlsize = 1000 // 1000mb
+//---------------------------------------------------------------------------
+
 cmd({  
      pattern: "tiktok",  
            alias :  ['tt','ttdl'],  
