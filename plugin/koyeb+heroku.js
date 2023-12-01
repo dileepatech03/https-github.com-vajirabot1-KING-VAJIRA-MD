8ae818366d38d8d11d7e6f49fdb10246a39cb9d7
DARK YASIYA <149582749+yasiyaofc1@users.noreply.github.com>
2023-12-01 09:53:33 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑 @yasiyaofc1

--- plugin/koyeb+heroku.js
+++ plugin/koyeb+heroku.js
@@ -1,12 +1,12 @@
 /**
- Copyright (C) 2022.
+ Copyright (C) 2023.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : @samapndey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
+ * @project_name : Vajira-Md
+ * @author : @yasiyaofc11 <https://github.com/yasiyaofc1>
+ * @description : Vajira,A Multi-functional whatsapp bot.
+ * @version 1.0.0
  **/
 const axios = require('axios');
 const { tlang,cmd } = require('../lib')
@@ -142,7 +142,7 @@ cmd(
                   [text.split(':')[0]]: text.split(':')[1],
           },
         });
-        await citel.reply(`🟩var ${text.split(':')[0]} : ${text.split(':')[1]} has been set Successfuly.`);
+        await citel.reply(`${text.split(':')[0]} : ${text.split(':')[1]} has been set Successfuly.`);
        }
        let check = await get_deployments()
        if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
