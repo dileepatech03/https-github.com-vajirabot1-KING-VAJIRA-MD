6650e7d964c1da14d4f02a574665f28323d5c14e
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-12-02 09:16:08 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- plugin/auto_voice_sender.js
+++ plugin/auto_voice_sender.js
@@ -12,7 +12,7 @@ const url = 'https://gist.github.com/vajirabot1/78e3877b7b0c04b9d197ec17fa2f5b47
         },
 **/ 
 cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
- 
+ if (Config.autoreaction === 'true' && citel.text)
   let { data } = await axios.get(url)
   for (vr in data){
  if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
