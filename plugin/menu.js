9ac3aeb9981ca0d35ca42b8fcd370596fcecae43
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-12-02 16:54:35 +0530

    Update menu.js

--- plugin/menu.js
+++ plugin/menu.js
@@ -38,7 +38,7 @@ cmd({
                 },
             ];
             let buttonMessage = {
-                image: {
+                mp4: {
                     url: 'https://telegra.ph/file/295038bbb664b4b65b0c2.mp4',
                 },
 
