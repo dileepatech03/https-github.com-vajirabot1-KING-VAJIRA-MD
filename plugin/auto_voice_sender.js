c40f31933587fc9a3c94fd82c0443c526141e1d7
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-12-02 09:06:35 +0530

    Update auto_voice_sender.js

--- plugin/auto_voice_sender.js
+++ plugin/auto_voice_sender.js
@@ -2,7 +2,7 @@ const { tlang,cmd } = require('../lib')
 const Config = require('../config')
 const axios = require('axios')
 // Put here your url with modified words and urls
-const url = 'https://gist.github.com/vajirabot1/400e4fe59b762555a98edb13603332dd/raw'
+const url = 'https://gist.github.com/vajirabot1/78e3877b7b0c04b9d197ec17fa2f5b47/raw'
 
 /**
  cmd({
@@ -12,6 +12,7 @@ const url = 'https://gist.github.com/vajirabot1/400e4fe59b762555a98edb13603332dd
         },
 **/ 
 cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
+ 
   let { data } = await axios.get(url)
   for (vr in data){
  if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
