51b1d6ffc1a8fecc10c087dc9ad3cadf4bc16a53
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-12-02 20:05:52 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- plugin/downloader.js
+++ plugin/downloader.js
@@ -43,6 +43,26 @@ cmd({
     )
      
      //---------------------------------------------------------------------------
+cmd({  
+     pattern: "tiktok",  
+     desc: "Downloads Tiktok Videos Via Url.",  
+     category: "downloader",  
+     react: "🎶",
+     filename: __filename,  
+     use: '<add tiktok url.>'  
+ },  
+  
+ async(Void, citel, text) => {  
+ if(!text) return await citel.reply(`*Uhh Please, Provide me tiktok Video Url*\n*_Ex .tiktok https://www.tiktok.com/@dakwahmuezza/video/7150544062221749531_*`);  
+ let txt = text ? text.split(" ")[0]:'';  
+ const { status , video, audio } = await tiktokdl(txt)  
+ if (status){ 
+     await Void.sendMessage(citel.chat, { audio: {url : audio,}, mimetype: 'audio/mpeg' }, { quoted: citel }); 
+     return await Void.sendMessage(citel.chat, {video : {url : video } , caption: "POWERD BY KING-VAJIRA" } , {quoted : citel }); 
+  }  
+ else return await citel.reply("Error While Downloading Your Video")   
+ })
+     //---------------------------------------------------------------------------
 
      cmd({
         pattern: "yts",
