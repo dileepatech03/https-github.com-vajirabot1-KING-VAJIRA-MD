9e5ab8b698bb8f9fdcc814194ed355d0bdfa2eaa
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-12-09 09:39:44 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/owner_info.js
+++ commands/owner_info.js
@@ -109,7 +109,7 @@ cmd({
             ];
             let buttonMessage = {
                 image: {
-                    url: 'https://telegra.ph/file/b10172f9cfdbe5f43c43e.jpg',
+                    url: 'https://telegra.ph/file/935bd7b1b967dabdf2ddf.jpg',
                 },
 
                 caption: `
