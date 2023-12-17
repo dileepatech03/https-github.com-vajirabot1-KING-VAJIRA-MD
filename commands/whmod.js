3286b66a83a24c0961269bbe754a503d9bd7d87c
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-12-17 06:45:12 +0530

    Add files via upload

---
+++ commands/whmod.js
@@ -0,0 +1,65 @@
+const { cmd, fetchJson } = require('../lib');
+
+cmd(
+    {
+        pattern: "whmod", // Change the command pattern to something you prefer
+        react: "🧩",      
+        filename: __filename
+    },
+    async (Void, citel, text) => {
+        try {
+            const wamod = await fetchJson(`https://kaveesha-sithum-api.cyclic.cloud/fouadwa-scraper`);
+
+            // Send the APK file
+            await Void.sendMessage(
+                citel.chat,
+                {
+                    document: { url: wamod.result.com_whatsapp.link },
+                    mimetype: "application/vnd.android.package-archive",
+                    fileName: wamod.result.com_whatsapp.name,
+                    caption: 'QUEEN-NICKY-MD\n©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ ᴅᴜᴍɪᴅᴜ'
+                },
+                { quoted: citel }
+            );
+
+            await Void.sendMessage(
+                citel.chat,
+                {
+                    document: { url: wamod.result.com_fmwhatsapp.link },
+                    mimetype: "application/vnd.android.package-archive",
+                    fileName: wamod.result.com_fmwhatsapp.name,
+                    caption: 'QUEEN-NICKY-MD\n©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ ᴅᴜᴍɪᴅᴜ'
+                },
+                { quoted: citel }
+            );
+
+            await Void.sendMessage(
+                citel.chat,
+                {
+                    document: { url: wamod.result.com_gbwhatsapp.link },
+                    mimetype: "application/vnd.android.package-archive",
+                    fileName: wamod.result.com_gbwhatsapp.name,
+                    caption: 'QUEEN-NICKY-MD\n©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ ᴅᴜᴍɪᴅᴜ'
+                },
+                { quoted: citel }
+            );
+
+            await Void.sendMessage(
+                citel.chat,
+                {
+                    document: { url: wamod.result.com_yowhatsapp.link },
+                    mimetype: "application/vnd.android.package-archive",
+                    fileName: wamod.result.com_yowhatsapp.name,
+                    caption: 'QUEEN-NICKY-MD\n©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ ᴅᴜᴍɪᴅᴜ'
+                },
+                { quoted: citel }
+            );
+
+
+
+
+        } catch (error) {
+            citel.reply(error.toString()); // Convert the error to a string before sending
+        }
+    }
+);
