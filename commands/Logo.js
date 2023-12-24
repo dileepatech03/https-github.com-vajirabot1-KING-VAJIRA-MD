7878a62f6333ec1673d3bc0bd8a16800bc21a687
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-12-24 16:50:08 +0530

    Update Logo.js

--- commands/Logo.js
+++ commands/Logo.js
@@ -13,7 +13,7 @@
  const maker = require('mumaker')
  let cap = `*_GENERATE BY KING-VAJIRA-MD_*`
      //---------------------------------------------------------------------------
-cmd({ pattern: "1", category: "thunder", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
+cmd({ pattern: "1", category: "thunder2", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
          if (!text) return citel.reply('_Need text._')
          let anu = await maker.textpro('https://textpro.me/create-thunder-text-effect-online-881.html', text)
          Void.sendMessage(citel.chat, { image: { url: anu.image }, caption: cap }, { quoted: citel })
